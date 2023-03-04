c3226cf4d42c7137c87678772a960631d6f77cbd
Adilet Zhuzupbek <52959571+AdiletZhuzupbekov@users.noreply.github.com>
2023-03-04 21:03:01 +0600

    initi

---
+++ src/test/java/kg/mara/babyfood/BabyFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.mara.babyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
